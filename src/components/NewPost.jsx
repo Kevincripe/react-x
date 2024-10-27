a34f7dc627b887e05bbdcf7b8d54f5b8ad90c783
Kevin Cripe <kevincripe6@gmail.com>
2024-10-27 11:55:54 -0500

    pause

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+
+  let enteredbody = '';
+  const stateData = useState('');
+  
+  function changeBodyHandler(event) {
+    enteredbody = event.target.value;
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{enteredbody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
 import classes from './PostsList.module.css';
+import NewPost from './NewPost';
 import Post from './Post';
 
 function PostsList() {
 
     return (
+        <>
+            <NewPost />
             <ul className={classes.posts}>
                 <Post author='Kevin' body='This is the first Post' />
                 <Post author='Ghost Writer' body='This is the random Post' />
             </ul>
+        </>
     );
 }
 
